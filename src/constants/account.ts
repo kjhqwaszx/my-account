cc55442ef4363c9f322c3a43deccf36eafd61e8d
jaehan <kyobo.jaehan@gmail.com>
2025-06-13 15:07:52 +0900

    계좌 정보 개발

---
+++ src/constants/account.ts
@@ -0,0 +1,13 @@
+export const ACCOUNT_STATUS = {
+  PROCESSING: 'READY',
+}
+
+export const ACCOUNT_MESSAGE = {
+  PROCESSING: '만들고 있으신\n계좌가 있으시군요',
+  NOT: '계좌 개설이\n더 쉽고 빨라졌어요',
+}
+
+export const ACCOUNT_BUTTON = {
+  PROCESSING: '이어만들기',
+  NOT: '3분만에 개설하기',
+}
