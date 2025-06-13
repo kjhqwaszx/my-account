195a11a5bf9a18ec2f595833b78e6b15833e7b69
jaehan <kyobo.jaehan@gmail.com>
2025-06-13 17:22:12 +0900

    신용정보 개발

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import dynamic from 'next/dynamic'
 
 import Account from '@components/home/Account'
 import { BannerSkeleton } from '@components/home/EventBanners'
+import { CreditScoreSkeleton } from '@components/home/CreditScore'
+import Spacing from '@shared/Spacing'
 
 const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
   //ssr 여부
@@ -9,12 +11,21 @@ const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
   loading: () => <BannerSkeleton />,
 })
 
+const CreditScore = dynamic(() => import('@components/home/CreditScore'), {
+  ssr: false,
+  loading: () => <CreditScoreSkeleton />,
+})
+
 export default function Home() {
   return (
     <>
       <EventBanners />
       {/*자산 영역은 ssr로 사용자에게 빠르게 노출시켜 준다.*/}
+
       <Account />
+      <Spacing size={8} backgroundColor="gray100" />
+
+      <CreditScore />
     </>
   )
 }
