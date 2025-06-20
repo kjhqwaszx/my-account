a4225cfc9d2047237e06634cb306c0bcae36b7db
jaehan <kyobo.jaehan@gmail.com>
2025-06-20 10:11:13 +0900

    next-auth 구글 기반 로그인 개발

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,21 +4,24 @@ import { QueryClientProvider, QueryClient, Hydrate } from 'react-query'
 
 import globalStyles from '@styles/globalStyles'
 import Layout from '@shared/layout'
+import { SessionProvider } from 'next-auth/react'
 
 const client = new QueryClient({})
 
 export default function App({
   Component,
-  pageProps: { dehydratedState, ...pageProps },
+  pageProps: { dehydratedState, session, ...pageProps },
 }: AppProps) {
   return (
     <Layout>
       <Global styles={globalStyles} />
-      <QueryClientProvider client={client}>
-        <Hydrate state={dehydratedState}>
-          <Component {...pageProps} />
-        </Hydrate>
-      </QueryClientProvider>
+      <SessionProvider session={session}>
+        <QueryClientProvider client={client}>
+          <Hydrate state={dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+        </QueryClientProvider>
+      </SessionProvider>
     </Layout>
   )
 }
---
+++ src/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,32 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { User } from '@/model/user'
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    // 로그인 성공 후 동작
+    session({ session, token }) {
+      console.log('session', session)
+      console.log('token', token)
+
+      if (session.user) {
+        ;(session.user as User).id = token.sub as string
+      }
+
+      return session
+    },
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    // 로그인 페이지 경로
+    signIn: '/auth/signin',
+  },
+})
